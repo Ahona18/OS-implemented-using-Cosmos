10343c5699738f65750e4bcbefdd07ab475f43df
Ahona <ahona18saha@gmail.com>
2021-08-21 15:00:42 +0530

    first push

---
+++ logo.cs
@@ -0,0 +1,30 @@
+﻿using Cosmos.HAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+
+
+
+namespace VLV_OS
+{
+	class logo
+	{
+		internal static void Logo()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine(" ------------------------------------------------------------------------------ ");
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WriteLine("|||||||||  ||     ||  |||||||||  ||\\     ||  |||||| ||||  ");
+			Console.WriteLine("||     ||  ||     ||  ||     ||  || \\    ||  ||       ||   ");
+			Console.WriteLine("||     ||  ||     ||  ||     ||  ||  \\   ||  ||       ||   ");
+			Console.WriteLine("|||||||||  |||||||||  ||     ||  ||   \\  ||  |||| ||||||   ");
+			Console.WriteLine("||     ||  ||     ||  ||     ||  ||    \\ ||  ||       ||   ");
+			Console.WriteLine("||     ||  ||     ||  ||     ||  ||     \\||  ||       ||   ");
+			Console.WriteLine("||     ||  ||     ||  |||||||||  ||     \\||  ||       ||   ");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine(" ------------------------------------------------------------------------------ ");
+		}
+	}
+}
---
+++ systemlock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace VLV_OS
+{
+	class sys_lock
+	{
+		public static void lockpass(string passcode)
+		{
+			string version = "2021-04-30";
+			bool unlocked = false;
+			while (!unlocked)
+			{
+				Console.Clear();
+				logo.Logo();
+				Console.WriteLine("                                                                                ");
+				Console.WriteLine("                                System Locked                                   ");
+				Console.WriteLine("                                                                                ");
+				Console.Write("Password: ");
+				string enterpass = Console.ReadLine();
+				if (enterpass == passcode)
+				{
+					unlocked = true;
+					Console.Clear();
+				}
+			}
+		}
+	}
+}
